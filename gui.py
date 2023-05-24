ddef334e53d2eea63f7b3eb9589fdeee0f067521
vlegaria <108449602+vlegaria@users.noreply.github.com>
2023-05-24 03:05:31 -0600

    Archivo principal gui.py comentado

--- gui.py
+++ gui.py
@@ -36,10 +36,11 @@ def process_text(text):
 
     return sentences, tokens, tagged, named_entities, filtered_words
 
-#Solo se buscaran archivos de texto de tipo .txt
-file_types = [("TXT (*.txt)", "*.txt"), ("All files (*.*)", "*.*")]
+# Para la parte de PLN solo se buscaran archivos de texto de tipo .txt
+file_types = [("TXT (*.txt)", "*.txt")]
 
-conf_layout = [ 
+# Configuración de los elementos de la interfaz (ubicación de los botones, textos de entrada y salida)
+layout = [ 
             [sg.Text('Escribe tus datos para guardarlos en las variables:')],
             [sg.Text("Nombre:"), sg.Input("", size=(40,1), key="-NOMBRE-"), sg.Text("Edad en años:"), sg.Input("", size=(5,1), key="-EDAD-")],
             [sg.Text("Altura en metros, ejemplo 1.50:"), sg.Input("", size=(5,1), key="-ESTATURA-"), sg.Text("¿Eres estudiante? Escribe True o False:"), sg.Input("", size=(5,1), key="-ESTUDIANTE-")],
@@ -52,82 +53,27 @@ conf_layout = [
             [sg.Text("Número:"), sg.Input("", size=(5,1), key="-RAIZ-"), sg.Button('CALCULAR RAIZ')],
             [sg.Text('Resultados:')], 
             [sg.Output(size=(50,7), key='-OUTPUT-')],
-            [
-                sg.Text("TXT File"),
-                sg.Input(
-                    size=(25, 1), key="-FILENAME-"
-                ),
-                sg.FileBrowse(file_types=file_types),
-                sg.Button("PROCESAR TEXTO"),
-            ],
-            [
-                sg.Text("Oraciones"),
-                sg.Combo(
-                    sentences,
-                    default_value=" ",
-                    size=(50,1),
-                    key="-SENTENCES-",
-                    enable_events=True,
-                    readonly=True,
-                )],
-            [
-                sg.Text("Tokens"),
-                sg.Combo(
-                    tokens,
-                    default_value=" ",
-                    size=(20,1),
-                    key="-TOKENS-",
-                    enable_events=True,
-                    readonly=True,
-                ),
-                sg.Text("Etiquetas gramaticales"),
-                sg.Combo(
-                    tagged,
-                    default_value=" ",
-                    size=(20,1),
-                    key="-TAGGED-",
-                    enable_events=True,
-                    readonly=True,
-                )
-            ],
-            [
-                sg.Text("Entidades nombradas"),
-                sg.Combo(
-                    named_entities,
-                    default_value=" ",
-                    size=(20,1),
-                    key="-NAMED_ENTITIES-",
-                    enable_events=True,
-                    readonly=True,
-                ),
-            
-                sg.Text("Palabras filtradas"),
-                sg.Combo(
-                    filtered_words,
-                    default_value=" ",
-                    size=(20,1),
-                    key="-FILTERED_WORDS-",
-                    enable_events=True,
-                    readonly=True,
-                ),
-            ],
+            [sg.Text("PRESIONA el boton para buscar archivo txt"), sg.Input(size=(25, 1), key="-FILENAME-"),sg.FileBrowse(file_types=file_types),sg.Button("PROCESAR TEXTO")],
+            [sg.Text("Oraciones"),sg.Combo(sentences,default_value=" ",size=(50,1),key="-SENTENCES-",enable_events=True, readonly=True )],
+            [sg.Text("Tokens"),sg.Combo(tokens,default_value=" ",size=(20,1),key="-TOKENS-",enable_events=True, readonly=True),
+                sg.Text("Etiquetas gramaticales"),sg.Combo( tagged,default_value=" ", size=(20,1), key="-TAGGED-", enable_events=True, readonly=True) ],
+            [sg.Text("Entidades nombradas"), sg.Combo(named_entities, default_value=" ",  size=(20,1), key="-NAMED_ENTITIES-", enable_events=True, readonly=True),
+                sg.Text("Palabras filtradas"),  sg.Combo( filtered_words, default_value=" ", size=(20,1), key="-FILTERED_WORDS-",  enable_events=True,readonly=True,),],
             [sg.Button('BORRAR TODO'), sg.Button('EXIT')],
     ]
 def main():
-    global confs
-    layout = [
-    [
-        sg.Column(conf_layout),
-    ]
-    ]
-
+    # Se crea el objeto de la ventana y se le asigna un nombre a la pestaña
     window = sg.Window('Tarea EBAC Valeria Legaria', layout)
-
-    while True:             # Event Loop
+    # La ventana se estará ejecutando hasta que se cierre o se de clic en el botón exit
+    while True: 
+        # Se agrega un bloque try-except para manejar que en caso de algún error, la ventana no se cierre
         try:
+            # Se leen los valores de los objetos de la interfaz (estatus de los botones y contenido de los espacios de entrada de texto)
             event, values= window.read()
+            # Si el botón EXIT es presionado, se termina el ciclo while
             if event in (sg.WIN_CLOSED, 'EXIT'):
                 break
+            # Si se presiona el botón "BORRAR TODO" se borra el contenido de los objetos en la interfaz
             if event == 'BORRAR TODO':
                 window['-OUTPUT-'].update('')
                 window['-NOMBRE-'].update('')
@@ -145,6 +91,9 @@ def main():
                 window["-TAGGED-"].Update(value="", values=[])
                 window["-NAMED_ENTITIES-"].Update(value="", values=[])
                 window["-FILTERED_WORDS-"].Update(value="", values=[])
+            # El botón GENERAR TEXTO Imprime en el cuadro de salida la variable de entrada siempre y cuando
+            # no sea un campo vació y coincida con el tipo de dato que se espera. En caso contrario se imprime
+            # un mensaje indicando el tipo de error
             if event == "GENERAR TEXTO":
                 window['-OUTPUT-'].update('')
                 if values["-NOMBRE-"]=="":
@@ -183,7 +132,8 @@ def main():
                     else:
                         print("Respuesta de Estudiante no válida, debe ser escrito como True o False")
                 
-
+            # El botón CONTADOR imprime el incremento de 1 en 1 de la variable contador hasta llegar 
+            # al valor indicado por el usuario 
             if event == "CONTADOR":
                 window['-OUTPUT-'].update('')
                 try:
@@ -194,6 +144,9 @@ def main():
                 except:
                     print("El valor debe ser numérico y entero")
 
+            # El botón IMPRIMIR imprime la oración "Me gusta la fruta" + el nombre de la fruta proveniente de la lista
+            # de frutas que ingrese el usuario. La oración se imprime cuantas frutas se hayan ingresado. Estas deben estar
+            # separadas por un punto y un espacio.
             if event == "IMPRIMIR":
                 window['-OUTPUT-'].update('')
                 try:
@@ -204,6 +157,8 @@ def main():
                 except:
                     print("La lista debe estar escrita de la forma: Pera. Uva. Fresa")
 
+            # El botón calcular área regresa el resultado de la operación matemática de multiplicar los valores númericos ingresados
+            # en los recuadros respectivos a base y altura.
             if event == "CALCULAR AREA":
                 try:
                     area = float(values["-BASE-"]) * float(values["-ALTURA-"])
@@ -211,6 +166,7 @@ def main():
                 except:
                     print("Los valores de base y altura deben ser numéricos")
                     
+            #l El botón CALCULAR RAÍZ regresa la raíz cuadrada del número ingresado por el usuario
             if event == "CALCULAR RAIZ":
                 try:
                     area = math.sqrt(float(values["-RAIZ-"]))
@@ -218,6 +174,9 @@ def main():
                 except:
                     print("El valor para cacular la raiz debe ser numérico")
 
+            # Para ejecutar el botón PROCESAR TEXTO primero es necesario buscar un archivo de texto .txt con el botón BROWSER
+            # Una vez que se visualiza la ruta del archivo en la interfas, al presionar el botón PROCESAR TEXTO se ejecuta la función 
+            # process_text la cual regresa listas después de aplicar distintos métodos de NPL al texto seleccionado
             if event == 'PROCESAR TEXTO':
                 window['-OUTPUT-'].update('')
                 filename = values["-FILENAME-"]
@@ -232,9 +191,10 @@ def main():
                 window["-NAMED_ENTITIES-"].Update(value=named_entities[0], values=named_entities)
                 window["-FILTERED_WORDS-"].Update(value=filtered_words[0], values=filtered_words)
         except:
+            # Se ignora si ocurrió algún error y se continúa en el bucle while
             pass
-
-
+    # Se cierra la ventana de la interfaz
     window.close()
+
 if __name__ == "__main__":
     main()
